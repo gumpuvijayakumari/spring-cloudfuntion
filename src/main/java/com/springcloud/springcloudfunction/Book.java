f5428f0d876f81c80d8893e486f87a4740e67bcf
Vijayakumari Gumpu <vijayakumari.gumpu@valuemomentum.com>
2023-02-22 11:52:23 +0530

    springcloud

---
+++ src/main/java/com/springcloud/springcloudfunction/Book.java
@@ -0,0 +1,32 @@
+package com.springcloud.springcloudfunction;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+	private int id;
+	public Book(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private String name;
+	
+
+}
